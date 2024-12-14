e14bcaf8e76e187509e017869b9a02d74ab763f9
Umbert Pensato Bosch <708948+umbertix@users.noreply.github.com>
2024-12-14 11:08:28 +0100

    fix(talos-iso): fix talos secure boot script

--- talos-iso.sh
+++ talos-iso.sh
@@ -8,6 +8,7 @@ fi
 read -p "Do you want secureboot? [Y/n] " sb_response
 sb_response=${sb_response:-Y}
 use_secureboot=false
+
 if [[ ${sb_response,,} =~ ^y ]]; then
     use_secureboot=true
 fi
@@ -18,9 +19,9 @@ if [[ $use_secureboot == true ]]; then
     talosctl gen secureboot uki --common-name "SecureBoot Key"
     talosctl gen secureboot pcr
     iso_type="secureboot-iso"
+    secure_boot_volume="-v $PWD/_out:/secureboot:ro"
 else
     iso_type="iso"
-    secure_boot_volume="-v $PWD/_out:/secureboot:ro"
 fi
 
 docker run --rm -t $secure_boot_volume -v $PWD/_out:/out ghcr.io/siderolabs/imager:v1.8.0 $iso_type $(echo "$extensions" | awk '{print "--system-extension-image " $1}' | tr '\n' ' ') --extra-kernel-arg net.ifnames=0 --extra-kernel-arg=-console --extra-kernel-arg=console=ttyS1
