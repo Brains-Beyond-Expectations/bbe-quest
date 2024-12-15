973c7f3275cdcef3affe9029326fbbad691e1f82
Nicolaj Vinholt <nicvspz@gmail.com>
2024-12-15 20:18:58 +0100

    feat: minor script improvements

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 VERSION=$(gh release list --limit 1 --json tagName -q '.[0].tagName')
 
 mkdir -p tmp
 
-gh release download $VERSION --repo nicolajv/bbe-quest --pattern "*linux-amd64.tar.gz" --dir ./tmp
+gh release download "$VERSION" --repo nicolajv/bbe-quest --pattern "*linux-amd64.tar.gz" --dir ./tmp
 
-tar -xvf ./tmp/bbe-$VERSION-linux-amd64.tar.gz
+tar -xvf ./tmp/bbe-"$VERSION"-linux-amd64.tar.gz
 
 rm -rf tmp
 
